62949ea299f3c6a324182ed90a5a6ce2fc53a3e8
shinokiwa <shinokiwa@gmail.com>
2024-02-22 10:20:55 +0000

    Web画面

--- yt_smtp_catcher/blueprints/mail.py
+++ yt_smtp_catcher/blueprints/mail.py
@@ -2,6 +2,7 @@
 メール送信ページ
 """
 from flask import Blueprint, render_template, current_app
+from pydantic import BaseModel
 
 from u_dam.sqlite3 import connect_database
 from database.tables.mails import get_mail
@@ -15,3 +16,26 @@ def mail():
     """
     return render_template('mail.html')
 
+class MailPost(BaseModel):
+    from: str
+    to: str
+    subject: str
+
+
+@bp.route('/mail', methods=['POST'])
+def mail_post():
+    """
+    メール送信
+    """
+    mail_id = request.form['mail_id']
+    mail = get_mail(mail_id)
+
+    # メール送信
+    current_app.mailer.send(
+        mail['from'],
+        mail['to'],
+        mail['subject'],
+        mail['body']
+    )
+
+    return render_template('mail.html')
