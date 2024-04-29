30d59bba8e8c6d1715045caa629c8c8141bffe45
shinokiwa <shinokiwa@gmail.com>
2024-04-29 04:53:33 +0000

    ディレクトリ構成変更

--- docker/main/entrypoint.sh
+++ docker/main/entrypoint.sh
@@ -5,8 +5,8 @@
 
 if [ "$DEBUG" = "1" ]; then
     # デバッグモードの場合はFlaskの開発サーバーを起動する
-    /usr/local/bin/python3 -m yt_smtp_catcher
+    sudo -u yt_server /usr/local/bin/python3 -m yt_smtp_catcher
 else
     # 本番モードの場合はGunicornを起動する
-    gunicorn -b 0.0.0.0 'yt_smtp_catcher:create_app()'
+    sudo -u yt_server gunicorn -b 0.0.0.0 'yt_smtp_catcher:create_app()'
 fi
